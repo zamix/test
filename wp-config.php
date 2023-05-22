5381eca449ff95112ea32f9d3d5ffeea24038c38
Sami Akkus <sami.akkus@socradar.io>
2023-05-22 11:33:32 +0300

    äbc

---
+++ wp-config.php
@@ -0,0 +1,23 @@
+<?php
+define('DB_NAME', 'your_database_name');
+define('DB_USER', 'your_database_username');
+define('DB_PASSWORD', 'your_database_password');
+define('DB_HOST', 'your_database_host');
+
+define('AUTH_KEY', 'your_auth_key');
+define('SECURE_AUTH_KEY', 'your_secure_auth_key');
+define('LOGGED_IN_KEY', 'your_logged_in_key');
+define('NONCE_KEY', 'your_nonce_key');
+define('AUTH_SALT', 'your_auth_salt');
+define('SECURE_AUTH_SALT', 'your_secure_auth_salt');
+define('LOGGED_IN_SALT', 'your_logged_in_salt');
+define('NONCE_SALT', 'your_nonce_salt');
+
+$table_prefix = 'your_table_prefix';
+
+define('WP_DEBUG', false);
+define('WP_AUTO_UPDATE_CORE', false);
+define('DISABLE_WP_CRON', true);
+
+define('WP_HOME', 'your_wordpress_home_url');
+define('WP_SITEURL', 'your_wordpress_siteurl');
